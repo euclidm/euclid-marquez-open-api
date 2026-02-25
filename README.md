d2518b21efd4551563a1bb1cbeba0206b21a62ed
yukisoba <marquezeuclidgwyne@gmail.com>
2026-02-24 23:23:21 -0800

    Added TheCatAPI fetch data

---
+++ README.md
@@ -0,0 +1,5 @@
+# euclid-marquez-open-ai
+
+# not sure which link:
+https://github.com/euclidm/euclid-marquez-open-api
+https://github.com/euclidm/euclid-marquez-open-api.git
---
+++ js/index.js
@@ -0,0 +1,17 @@
+const API_URL = `https://api.thecatapi.com/v1/`;
+const API_KEY = `live_sbonU1ePugWREraEOeaiPPhbTUecRDcQpH3vAvRxqbwMg531RSI0X82wAvEcc6un`;
+
+// TheCatAPI Fetch
+fetch(`${API_URL}breeds`, {headers: {"x-api-key": API_KEY}})
+
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+    console.log("TheCatAPI Response: ", data);
+})
+
+.catch(function(error){
+    console.error("Error fetching TheCatAPI data: ", error);
+});
