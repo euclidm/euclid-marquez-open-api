9435a8be4aca3aeaee2f2e7dc846548c7b6688e3
yukisoba <marquezeuclidgwyne@gmail.com>
2026-03-03 21:40:17 -0800

    Added CSS Properties + Fetched 2nd Data

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const API_URL = `https://api.thecatapi.com/v1/`;
 const API_KEY = `live_sbonU1ePugWREraEOeaiPPhbTUecRDcQpH3vAvRxqbwMg531RSI0X82wAvEcc6un`;
 
-// TheCatAPI Fetch Breeds
+// [1] TheCatAPI Fetch Breeds
 fetch(`${API_URL}breeds`, {headers: {"x-api-key": API_KEY}})
 
 .then(function(response){
@@ -9,9 +9,29 @@ fetch(`${API_URL}breeds`, {headers: {"x-api-key": API_KEY}})
 })
 
 .then(function(data){
-    console.log("TheCatAPI Response: ", data);
+    console.log("TheCatAPI (Breed) Response: ", data);
 })
 
 .catch(function(error){
-    console.error("Error fetching TheCatAPI data: ", error);
+    console.error("Error fetching TheCatAPI (Breed) data: ", error);
+});
+
+// [3] TheCatAPI Fetch Images
+fetch(`${API_URL}images/search?limit=${12}&has_breeds=1`, {headers: {"x-api-key": API_KEY}})
+
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+    console.log("TheCatAPI (Imgs) Response: ", data);
+
+    data.forEach((cat, index) => {
+        const cat_breed = cat.breeds?.[0];
+        console.log(`Cat #${index + 1}: `, "Breed - ", cat_breed?.name, " Origin - ", cat_breed?.origin, " Image - ", cat?.url);
+    })
+})
+
+.catch(function(error){
+    console.error("Error fetching TheCatAPI (Imgs) data: ", error);
 });
